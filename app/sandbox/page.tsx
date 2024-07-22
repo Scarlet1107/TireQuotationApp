8f1371d5f37a261b710502166ac7a939cdcb52f5
scarlet7 <shougo.ushiro@gmail.com>
2024-07-22 14:01:30 +0900

    npm i ReactToPrint and test functions in sandbox routing

---
+++ app/sandbox/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { useRef } from "react";
+import ReactToPrint from "react-to-print";
+import { ComponentToPrint } from "./ComponentToPrint";
+
+const App = () => {
+  const componentRef = useRef(null);
+  const [content, setContent] = React.useState(100);
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => setContent(content + 100)}
+          className="ml-4 mt-4 flex justify-center rounded bg-blue-500 px-4 py-2 hover:bg-blue-600"
+        >
+          Click to increase number
+        </button>
+        <p className="text-xl">{content}</p>
+        <ReactToPrint
+          trigger={() => (
+            <button className="ml-4 mt-4 flex justify-center rounded bg-blue-500 px-4 py-2 hover:bg-blue-600">
+              Print this out!
+            </button>
+          )}
+          
+          content={() => componentRef.current}
+        />
+      </div>
+      <div className="border-2 w-1/2" style={{ display: "none" }}>
+        <ComponentToPrint ref={componentRef} content={content} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
