dd5133d2f38acdabd4ad3d1024bc42b1be55cdc0
Scarlet1107 <shougo.ushiro@gmail.com>
2024-08-15 12:29:06 +0900

    Change comment

--- config/constants.ts
+++ config/constants.ts
@@ -2,7 +2,7 @@
 
 import { CheckboxState, DiscoundRate } from "@/utils/interface";
 
-export const TAX_RATE = 1.1; // 消費税率
+export const TAX_RATE = 1.1; // 消費税率。2024年8月時点で10%
 export const DEFAULT_EXPIRY_DATE = 14 * 24 * 60 * 60 * 1000; // 見積もりの有効期限。デフォルトは14日
 
 // 作業工賃、脱着工賃、タイヤ預かり料の割引率（%設定）
@@ -12,7 +12,7 @@ export const DEFAULT_DISCOUNT_RATE = <DiscoundRate>{
   tireStorageFee: 100,
 };
 
-// チェックボックスの初期値
+// 工賃のチェックボックスの初期値
 export const DEFAULT_CHECKED_STATUS = <CheckboxState>{
   laborFee: true,
   removalFee: true,
