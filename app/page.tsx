9c3676cdc768f56e39741905dbd817c718adfb8e
Scarlet1107 <shougo.ushiro@gmail.com>
2024-10-14 14:55:58 +0900

    Compress quotation Number

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const Main = () => {
   const generateQuotationNumber = (): string => {
     const now = new Date();
 
-    const year = now.getFullYear();
+    const year = String(now.getFullYear()).slice(-2);
     const month = String(now.getMonth() + 1).padStart(2, "0");
     const day = String(now.getDate()).padStart(2, "0");
     const hours = String(now.getHours()).padStart(2, "0");
